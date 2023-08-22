a3f681bdcdec466c5de2c3189a6977b350185ce5
IcyTapcart <142934559+IcyTapcart@users.noreply.github.com>
2023-08-22 15:51:21 -0700

    Update pricing.js

--- pricing.js
+++ pricing.js
@@ -1,3 +1,15 @@
-const testFn = function(){
-  console.log("loaded js file from github")
+function calc_d_num(num) {
+  console.log("calc_d_num loaded from gh")
+  var num, d_num;
+  if (num <= 30) {
+    d_num = num / 10;
+  } else if (num <= 50) {
+    d_num = (num - 30) * 0.35 + 3;
+  } else if (num <= 70) {
+    d_num = (num - 50) * 2 + 10;
+  } else if (num <= 100) {
+    d_num = ((num - 70) * 5) / 3 + 50;
+  }
+  d_num = parseFloat(d_num.toFixed(3));
+  return d_num === 0 ? d_num : d_num + "M";
 }
